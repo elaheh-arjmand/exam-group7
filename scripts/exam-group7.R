6b467868c8d9bda1d27ddbd6213f17bac9bfa522
anemarthe <seh003@uib.no>
2024-09-09 13:32:54 +0200

    Adding name to header

--- scripts/exam-group7.R
+++ scripts/exam-group7.R
@@ -1,6 +1,6 @@
 #----Script Header-------------------------####
 # Date:09.09.2024         
-# Author: Elaheh Javadi Arjmand      
+# Author: Elaheh Javadi Arjmand, Ane Helland     
 # Filename: exam-group7    
 # Description:  group work for RMED901 course
 #               
