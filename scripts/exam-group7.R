2f41bc9c5485bffad0b22d8674d343a27a48a9ce
elaheh-arjmand <elaheh.arjmand@gmail.com>
2024-09-09 13:16:08 +0200

    Creating a new script

---
+++ scripts/exam-group7.R
@@ -0,0 +1,9 @@
+#----Script Header-------------------------####
+# Date:09.09.2024         
+# Author: Elaheh Javadi Arjmand      
+# Filename: exam-group7    
+# Description:  group work for RMED901 course
+#               
+#               
+# Project: exam-group7
+#-------------------------------------------###
