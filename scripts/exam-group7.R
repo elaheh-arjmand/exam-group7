67caa996e9d56a798babea40545f2e5e55bacaac
anemarthe <seh003@uib.no>
2024-09-09 14:35:06 +0200

    Removing duplications

--- scripts/exam-group7.R
+++ scripts/exam-group7.R
@@ -16,3 +16,26 @@ read_delim(here("data", "exam_dataset.txt"))
 
 #assign name to variable
 dataset <- read_delim(here("data", "exam_dataset.txt"))
+
+#view data
+head(dataset)
+tail(dataset)
+glimpse(dataset)
+
+#Removing duplicates ----
+#possible functions to use:
+?distinct
+?duplicated
+
+#Finding duplicates
+duplicated(dataset)
+#Result: Provides a value of FALSE or TRUE for all rows. For all rows after 297, this returns the value "TRUE" 
+#All rows from 298-387 are duplicates...
+
+#Removing duplicates be retaining only the first version of the duplicated row.
+dataset_nodupliactions <- distinct(dataset)
+#Resulting in table of 297 x 14, seems correct.
+#Naming the new dataset with no duplications as "dataset_noduplications"
+
+
+
