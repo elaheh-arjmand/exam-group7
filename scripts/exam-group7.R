081baa3d481cdce7297d0e3c6d1cb7474730d0b9
elaheh-arjmand <elaheh.arjmand@gmail.com>
2024-09-09 15:06:29 +0200

    finalizing the merge, fixing conflicts on day 5

--- scripts/exam-group7.R
+++ scripts/exam-group7.R
@@ -32,13 +32,13 @@ duplicated(dataset)
 #Result: Provides a value of FALSE or TRUE for all rows. For all rows after 297, this returns the value "TRUE" 
 #All rows from 298-387 are duplicates...
 
-#Removing duplicates be retaining only the first version of the duplicated row.
+#Removing duplicates be retaining only the first version of the duplicated row
 dataset_nodup <- distinct(dataset)
 #Resulting in table of 297 x 14, seems correct.
 #Naming the new dataset with no duplications as "dataset_noduplications"
 
 
-#difficulties intubating needs to be separated into bleeding, ease & sore_throat
+#difficulties intubating needs to be separated into bleeding, ease & sore_throat----
 dataset_nodup %>% 
   pivot_wider(names_from = difficulties_intubating_feature, 
               values_from = difficulties_intubating_value)
@@ -53,19 +53,19 @@ dataset_nodup_sep
 view(dataset_nodup_sep)
 
 
-#fixing column names, removing spases and putting everything in lowercase
+#fixing column names----
 glimpse(dataset_nodup_sep)
-dataset<-
-  dataset %>%
+dataset_nodup_sep<-
+  dataset_nodup_sep %>%
   rename(bmi= `BMI kg/m2`,
          randomization= Randomization,
          attempts= `# attempts`,
          failures= `# failures`)
 
-#Author: SM, Date: 09.09.24, Time: 14.45
+#Author: SM, Date: 09.09.24, Time: 14.45----
 #Separate the data in column "last_method_S_F" 
 #One new column called "intubation method" and one with "intubation success"
-dataset %>% 
+dataset_nodup_sep<- dataset_nodup_sep %>% 
   separate(col = "last_method_S_F", 
            into = c("intibation method", "intubation success"), 
            sep = "_")
