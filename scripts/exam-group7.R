6009b9c2ace59172132a18e39a662fad2e809f91
hiestandhere <stand.hiestand@gmail.com>
2024-09-09 14:37:39 +0200

    separating difficulties_intubating_feature into bleeding, ease & sore_throat needs Ane's variable for the dataset before it will run without duplicates

--- scripts/exam-group7.R
+++ scripts/exam-group7.R
@@ -16,3 +16,17 @@ read_delim(here("data", "exam_dataset.txt"))
 
 #assign name to variable
 dataset <- read_delim(here("data", "exam_dataset.txt"))
+
+#difficulties intubating needs to be separated into bleeding, ease & sore_throat
+dataset %>% 
+  pivot_wider(names_from = difficulties_intubating_feature, 
+              values_from = difficulties_intubating_value)
+
+summary(dataset$difficulties_intubating_value)
+dataset$difficulties_intubating_value
+
+test_separation <- dataset %>% 
+  pivot_wider(names_from = difficulties_intubating_feature, 
+              values_from = difficulties_intubating_value)
+test_separation
+view(test_separation)
