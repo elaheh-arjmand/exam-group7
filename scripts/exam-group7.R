107fa7054c15a42718a89088638cd0c6f087d80f
hiestandhere <stand.hiestand@gmail.com>
2024-09-09 13:42:52 +0200

    I too added my name! Hurrah!

--- scripts/exam-group7.R
+++ scripts/exam-group7.R
@@ -1,6 +1,6 @@
 #----Script Header-------------------------####
 # Date:09.09.2024         
-# Author: Elaheh Javadi Arjmand, Ane Helland, Sigurd Mydske     
+# Author: Elaheh Javadi Arjmand, Ane Helland, Sigurd Mydske, Stand Hiestand     
 # Filename: exam-group7    
 # Description:  group work for RMED901 course
 #               
