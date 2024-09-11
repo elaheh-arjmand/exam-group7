bd970927d53992e4ffe2ed9da1d2141614d76c02
anemarthe <seh003@uib.no>
2024-09-11 09:11:48 +0200

    Update table

--- scripts/table.R
+++ scripts/table.R
@@ -11,4 +11,10 @@ count(myData_columns, conformity_rand_int)
 
 table(myData_columns$gender_characters, myData_columns$conformity_rand_int)
 
-#Makes a table of gender (female/ male) vs "is randomization == intubation method" listed as true or false. 
+#Makes a matrix of gender (female/ male) vs "is randomization == intubation method" listed as true or false. 
+
+myData_columns %>%
+  group_by(gender_characters, conformity_rand_int) %>%
+  count()
+#Makes a table
+#Here the output is listed in a table whereas on the first try above the output is really a matrix. 
